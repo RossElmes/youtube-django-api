a0c22e2757cf0443cd8f2b54bc41a0fddf24939f
RossElmes <rosselmes@hotmail.com>
2024-07-26 20:21:52 +0100

    Update the start and end times to be more than 5 digits

--- youtube/models.py
+++ youtube/models.py
@@ -33,8 +33,8 @@ class MatchDetail(models.Model):
 
 class MatchClip(models.Model):
     match = models.ForeignKey(MatchDetail, related_name='match',on_delete=models.CASCADE)
-    start_time = models.DecimalField(max_digits=5, decimal_places=2)
-    end_time = models.DecimalField(max_digits=5, decimal_places=2)
+    start_time = models.DecimalField(max_digits=10, decimal_places=2)
+    end_time = models.DecimalField(max_digits=10, decimal_places=2)
     name = models.CharField(max_length=100)
 
     def __str__(self):
