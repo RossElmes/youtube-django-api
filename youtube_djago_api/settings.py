249271a6a27c94c1bc62cfba3cd260c529126cc0
RossElmes <rosselmes@hotmail.com>
2024-07-26 22:32:33 +0100

    Update allowed hosts

--- youtube_djago_api/settings.py
+++ youtube_djago_api/settings.py
@@ -29,8 +29,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG','False') == 'True'
 #DEBUG = True
 
-#ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
+#ALLOWED_HOSTS = []
 
 
 # Application definition
