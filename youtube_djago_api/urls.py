d81a6090f985656bf19e0dbdab7829a45dc68d87
RossElmes <rosselmes@hotmail.com>
2024-08-06 20:34:33 +0100

    update to have authentication

--- youtube_djago_api/urls.py
+++ youtube_djago_api/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path ,include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include('youtube.urls'))
+    path('api/',include('youtube.urls')),
+    path('api/', include('authapp.urls')),
 ]
